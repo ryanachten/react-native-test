426d58f7a4a2ebb36a04d9290bf284738e963585
Ryan Achten <ryanachten@gmail.com>
2018-11-04 14:36:26 +1300

    Add axios for http requests, Add album get request to AlbumList

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      albums: []
+    };
+  }
+
   componentWillMount() {
-    console.log('mount yo!');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response) => {
+      this.setState({
+        albums: response.data
+      });
+    });
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(({ title }) => (
+      <Text key={title}>{title}</Text>
+    ));
   }
 
   render() {
     return (
       <View>
-        <Text>AlbumList</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
