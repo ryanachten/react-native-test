dbfef572319298bf15842285ecad1790d165d5ce
Ryan Achten <ryanachten@gmail.com>
2018-11-05 19:03:05 +1300

    Add image and artist to AlbumDetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, Text, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
+const styles = {
+  headerStyles: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
+};
+
 export default ({ album }) => {
-  const { title } = album;
+  const { contentStyle, thumbnailStyle } = styles;
+  const { title, artist, thumbnail_image } = album;
   return (
     <Card>
       <CardSection>
-        <Text>{title}</Text>
+        <View>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={contentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
