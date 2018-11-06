994938f0588b42b8f3da02b84e3e56797bfd041a
Ryan Achten <ryanachten@gmail.com>
2018-11-06 18:37:17 +1300

    Add full width album artwork to AlbumDetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,32 +4,55 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const styles = {
-  headerStyles: {
+  headerStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  titleStyle: {
+    fontSize: 18
   },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  albumImageStyle: {
+    flex: 1,
+    height: 300,
+    width: null
   }
 };
 
 export default ({ album }) => {
-  const { contentStyle, thumbnailStyle } = styles;
-  const { title, artist, thumbnail_image } = album;
+  const {
+    headerStyle, thumbnailStyle, thumbnailContainerStyle,
+    titleStyle, albumImageStyle
+  } = styles;
+  const { title, artist, thumbnail_image, image } = album;
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
-        <View style={contentStyle}>
-          <Text>{title}</Text>
+        <View style={headerStyle}>
+          <Text style={titleStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={albumImageStyle}
+          source={{ uri: image }}
+        />
       </CardSection>
     </Card>
   );
